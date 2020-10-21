c3656098543ec5728aaca0c65f33cf78dc533899
Hakan Han <eenestatli@gmail.com>
2020-10-21 12:58:28 +0300

    added necessary packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    semi: 'off',
+    'no-shadow': 'off',
+    'no-alert': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react-native/no-inline-styles': 'off',
+  },
 };
