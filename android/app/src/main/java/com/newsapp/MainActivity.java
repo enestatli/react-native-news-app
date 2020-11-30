d73d9206f265872d349924f2ed3c3de97c89569a
Hakan Han <eenestatli@gmail.com>
2020-11-30 18:41:19 +0300

    added server folder and splash screen

--- android/app/src/main/java/com/newsapp/MainActivity.java
+++ android/app/src/main/java/com/newsapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.cekmecem.newsapp;
 
+import android.os.Bundle; 
+
 import com.facebook.react.ReactActivity;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "NewsApp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
   }
 }
