b4acdc9df472ef1d6e4c09336ce66fc0edf7d335
enestatli <eenestatli@gmail.com>
2021-01-30 23:29:19 +0300

    added static func / hashing password before saving db

--- server/db/models/userSchema.js
+++ server/db/models/userSchema.js
@@ -8,4 +8,23 @@ const User = new Schema({
   password: String,
 });
 
+User.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+User.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+};
+
+
 module.exports = mongoose.model('user', User);
--- server/index.js
+++ server/index.js
@@ -5,10 +5,15 @@ require('dotenv').config();
 
 const { config } = require('./config');
 const { logger } = require('./logger');
+const router = require('./api')
 
 const app = express();
 
+require('./db')
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(router)
 
 app.listen(config.port, () => logger.info(`Express is on ${config.port}`));
