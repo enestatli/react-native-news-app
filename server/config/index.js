14b8acd849e264fe34b36ce000da8d8b3ac489ff
Hakan Han <eenestatli@gmail.com>
2021-01-30 20:22:12 +0300

    server config is updated

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,23 @@
-const dev = {};
+const dev = {
+  baseUrl: process.env.BASE_URL,
+  apiKey1: process.env.APIKEY_1,
+  apiKey2: process.env.APIKEY_2,
+  port: process.env.PORT,
+};
 
-const prod = {};
+const prod = {
+  apiKey3: process.env.APIKEY_3,
+};
 
-const joint = {};
+const config = {
+  dbUri: process.env.DB_URI || 'mongodb://localhost:27017',
+  dbName: process.env.DB_NAME,
+  newsUrl: process.env.NEWS_URL,
+};
 
-module.exports = {};
+const all =
+  process.env.NODE_ENV === 'development'
+    ? { ...config, ...dev }
+    : { ...config, ...prod };
+
+module.exports = { config: all };
