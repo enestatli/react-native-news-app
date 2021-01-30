0344ce1a1a9dcf4f3e6b47e9a3225b8fe786bbcd
Hakan Han <eenestatli@gmail.com>
2021-01-30 20:38:22 +0300

    added necessary pckgs

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const { config } = require('./config');
+const { logger } = require('./logger');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello fwfw');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.listen(3000);
+app.listen(config.port, () => logger.info(`Express is on ${config.port}`));
