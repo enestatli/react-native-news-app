8efaa000636126b16389b3b35ebc61741aef174f
Cihangir Bozdogan <cihangirrbozzd@gmail.com>
2021-01-30 12:47:26 +0300

    Post request handling

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/user/:id/hair/:category', (req, res) => {
-  console.log(req.params.category);
-  console.log(req.query.age);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/test', (req, res) => {
+  res.json(req.body);
 });
 
 app.listen(3000, () => console.log('Express is on 3000'));
