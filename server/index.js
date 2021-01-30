b0c30bb6ad50915de7b1f8b063b2a34669675e98
Cihangir Bozdogan <cihangirrbozzd@gmail.com>
2021-01-30 12:37:19 +0300

    Request params handling

--- server/index.js
+++ server/index.js
@@ -2,16 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/user', (request, response, next) => {
-  response.send('User route is on');
-});
-
-app.get('/admin', (request, response, next) => {
-  if (request && request.query && request.query.msg) {
-    response.send(request.query.msg);
-    return;
-  }
-  response.send('Msg yok');
+app.get('/user/:id/hair/:category', (req, res) => {
+  console.log(req.params.category);
+  console.log(req.query.age);
 });
 
 app.listen(3000, () => console.log('Express is on 3000'));
