2d82066897c41eb0c7a44feaba33070d3ca6bf3a
Hakan Han <eenestatli@gmail.com>
2021-01-30 21:00:16 +0300

    connected to db

---
+++ server/db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const { config } = require('../config');
+const { logger } = require('../logger');
+
+mongoose
+  .connect(`${config.dbUri}/${config.dbName}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info(`Connected to mongodb: ${config.dbUri}/${config.dbName}`);
+  });
+
+mongoose.connection.on('error', (err) => {
+  logger.error(err);
+});
