f1091cb2792844e2d24f90202582ae70111f9c25
enestatli <eenestatli@gmail.com>
2021-02-05 23:38:30 +0300

    minor bug

--- server/api/index.js
+++ server/api/index.js
@@ -4,9 +4,13 @@ const router = express.Router();
 
 const { User } = require('./User');
 const { News } = require('./News');
+const { Comment } = require('./Comment');
+
 
 new User(router);
 
 new News(router);
+
+new Comment(router)
 
 module.exports = router;
